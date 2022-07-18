1b0c443709b25b517fd0f7df7c006a992bf1cfa3
jygaulier <gaulier@alchemy.fr>
2022-07-18 17:55:07 +0200

    WIP

--- lib/PHPExiftool/PHPExiftool.php
+++ lib/PHPExiftool/PHPExiftool.php
@@ -2,10 +2,24 @@
 
 namespace PHPExiftool;
 
+use Psr\Log\NullLogger;
+
 class PHPExiftool
 {
     public static function getKnownTagGroups()
     {
         return include __DIR__ . "/Driver/TagGroup/index.php";
+    }
+
+    public static function generateClasses(bool $withMwg = true, array $lngs = ['en'])
+    {
+        $options = [];
+        if ($withMwg) {
+            $options[] = InformationDumper::LISTOPTION_MWG;
+        }
+
+        $dumper = new InformationDumper(new Exiftool(new NullLogger()));
+
+        $dumper->dumpClasses($lngs);
     }
 }
