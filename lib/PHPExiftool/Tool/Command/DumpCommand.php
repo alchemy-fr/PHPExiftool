4c30dc4aa959a5ca606242d197ee80d76060a0a4
jygaulier <gaulier@alchemy.fr>
2022-07-25 15:58:04 +0200

    move dom-crawler as required (not only in dev) jor jit classes generation better dump cli

--- lib/PHPExiftool/Tool/Command/DumpCommand.php
+++ lib/PHPExiftool/Tool/Command/DumpCommand.php
@@ -1,48 +1,30 @@
 <?php
 
-/**
- * This file is part of the PHPExiftool package.
- *
- * (c) Alchemy <support@alchemy.fr>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace PHPExiftool\Tool\Command;
 
 use Exception;
 use PHPExiftool\Driver\Metadata\Metadata;
+use PHPExiftool\PHPExiftool;
 use PHPExiftool\Reader;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 
-/**
- *
- * @author      Romain Neutron - imprec@gmail.com
- * @license     http://opensource.org/licenses/MIT MIT
- */
 class DumpCommand extends Command
 {
     protected InputInterface $input;
     protected OutputInterface $output;
-
-    /**
-     *
-     * @var array
-     */
-    protected array $classes = [];
 
     protected function configure()
     {
         $this
             ->setName('dump')
             ->setDescription('Dump metadata from a file')
-            ->addArgument('file', InputArgument::REQUIRED, 'The file')
-
+            ->addOption('filter', null, InputOption::VALUE_REQUIRED, "Dump only infos for Id's matching this regexp, e.g. \"^(XMP|FILE)\"")
+            ->addArgument('file', InputArgument::OPTIONAL, 'The file')
         ;
 
         return $this;
@@ -53,33 +35,49 @@ class DumpCommand extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $output->writeln("hello");
-
-        $start = microtime(true);
-
         $this->input = $input;
         $this->output = $output;
 
-        $logger = new \Symfony\Bridge\Monolog\Logger("PHPExiftool");
-        $reader = Reader::create($logger);
-        $reader->files($input->getArgument('file'));
-        $metadataBag = $reader->files(__FILE__)->first();
-
-        /**
-         * @var Metadata $meta
-         */
-        foreach($metadataBag as $meta) {
-            $tag = $meta->getTag();
-            $output->writeln(sprintf("<info>%s</info> (name=\"%s\", phpType=\"%s\") ; %s", $tag->getId(), $tag->getName(), $tag->getPhpType(), $tag->getDescription('en')));
-            $output->write($tag->isMulti() ? " multi" : " mono");
-            $output->write($tag->isBinary() ? " binary" : "");
-            $output->write($tag->isWritable() ? " writable" : " read-only");
-            $output->writeln($tag->getMaxLength() !== 0 ? (" maxl=" . $tag->getMaxLength()) : "");
-
-            $v = $meta->getValue();
-            $output->writeln(sprintf(" value: \"%s\"", $v->asString()));
+        if(is_null($filter = $input->getOption('filter'))) {
+            $filter = '';
         }
+        $filter = '/' . $filter . '/';
+        /**
+         * dump the meta from a file
+         */
+        if($input->getArgument('file')) {
 
+            $logger = new \Symfony\Bridge\Monolog\Logger("PHPExiftool");
+            $reader = Reader::create($logger);
+            $reader->files($input->getArgument('file'));
+            $metadataBag = $reader->files(__FILE__)->first();
+
+            /**
+             * @var Metadata $meta
+             */
+            foreach ($metadataBag as $meta) {
+                $tag = $meta->getTag();
+                $id = $tag->getId();
+                if(preg_match($filter, $id)) {
+                    $output->writeln(sprintf("<info>%s</info> (name=\"%s\", phpType=\"%s\") ; %s", $id, $tag->getName(), $tag->getPhpType(), $tag->getDescription('en')));
+                    $output->write($tag->isMulti() ? " multi" : " mono");
+                    $output->write($tag->isBinary() ? " binary" : "");
+                    $output->write($tag->isWritable() ? " writable" : " read-only");
+                    $output->writeln($tag->getMaxLength() !== 0 ? (" maxl=" . $tag->getMaxLength()) : "");
+
+                    $v = $meta->getValue();
+                    $output->writeln(sprintf(" value: \"%s\"", $v->asString()));
+                }
+            }
+        }
+        else {
+            // no file arg ? dump the dictionnary
+            foreach(PHPExiftool::getKnownTagGroups() as $tagGroup) {
+                if(preg_match($filter, $tagGroup)) {
+                    $output->writeln($tagGroup);
+                }
+            }
+        }
         return 0;
     }
 
